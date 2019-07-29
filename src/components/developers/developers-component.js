29ab06dda9c669771156c22d52fc58681a94c557
Volha Zakharava <zakharava.volha@gmail.com>
2019-07-29 22:35:04 +0300

    feat: add page for developers

--- src/components/developers/developers-component.js
+++ src/components/developers/developers-component.js
@@ -1,7 +1,22 @@
 import React from 'react';
+
+import "./developers.css"
+import { Developeritem } from "./developer-item"
+import {developersList} from '../../data/developers-data'
+
 export const Developers = () => (
 
-<div>
-developers block
+<div className="developers-container">
+  <div>Title</div>
+  {developersList.map(item => (
+  <div className="developers-item">  
+    <Developeritem 
+      name={item.name}
+      image={item.photoUrl}
+      github={item.github}
+      githubName={item.githubName}
+
+    />    
+  </div>))}
 </div>
 );
