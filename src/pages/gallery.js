d9ba252be5dce9997502fc9689ad2921ade7c4cd
Artsiom <43322812+Artsiom-Zhuk@users.noreply.github.com>
2019-07-30 00:43:23 +0300

    feat: implementation photo gallery (#11)

---
+++ src/pages/gallery.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Translation } from 'react-i18next';
+import { withI18next } from '@wapps/gatsby-plugin-i18next';
+
+import PhotosGallery from '../components/photo-gallery/index';
+import { producers } from '../data/producers'
+
+class Gallery extends React.Component {
+  render() {
+    return (
+      <Translation>
+        {t => (
+          <PhotosGallery producer={producers[0]}/>
+        )}
+      </Translation>
+    )
+  }
+}
+
+export default withI18next()(Gallery);
+
+export const query = graphql`
+  query($lng: String!) {
+    locales: allLocale(filter: { lng: { eq: $lng }, ns: { eq: "messages" } }) {
+      ...LocaleFragment
+    }
+  }
+`;
