16f6d2efacaff5d70833fe807252205b549d7fe2
Mitskevich Victor <mv2.mx@yandex.ru>
2019-07-29 17:46:57 +0300

    feat: add timeline

---
+++ src/components/timeline/timeLine.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Timeline }  from 'vertical-timeline-component-for-react';
+import { TimeItem } from './timeline-item/TimeLineItem';
+import { producers } from '../../data/producers';
+import '../timeline/timeline-item/TimeLineItem.scss';
+
+
+export const TimeLineFull = ( {id} ) => {
+
+  const biographyData = producers[id].biography;
+
+  return (
+  <Timeline>
+    <TimeItem biography={ biographyData }/>
+  </Timeline>
+  );
+};
