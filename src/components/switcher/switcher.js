42c78d77813d70c3a8eaa67aa1a1fcfb35a75e49
Siarhey Kiniou <netralitet@gmail.com>
2019-07-29 11:30:06 +0300

    feat: active language style switch

--- src/components/switcher/switcher.js
+++ src/components/switcher/switcher.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Language } from '@wapps/gatsby-plugin-i18next';
 
+import './switcher.css'
+
 const Switcher = ({ changeLng, lng, availableLngs }) => (
   <ul style={{ listStyle: 'none' }}>
     {availableLngs.map(value => (
       <li key={value} style={{ display: 'inline' }}>
-        <button
-          style={{
-            background: 'rebeccapurple',
-            color: 'white',
-            textDecoration: value === lng ? 'underline' : 'none',
-          }}
+        <button className = {(value === lng) ? 'active-btn-lang': 'btn-lang'}
           onClick={() => changeLng(value)}
         >
           {value}
