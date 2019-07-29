0041543314f63f1f819120a9a7b82d285147ad1e
Volha Zakharava <zakharava.volha@gmail.com>
2019-07-29 22:37:49 +0300

    fix: change index page to dev team page

--- src/pages/dev-team.js
+++ src/pages/dev-team.js
@@ -10,7 +10,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>    
+    </div>
+    <Developers />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
