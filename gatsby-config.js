860d44897a0f9878bd8e71de572549f277c49c7b
Siarhey Kiniou <netralitet@gmail.com>
2019-07-29 10:53:24 +0300

    feat: add gatsby-plugin-i18next

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,23 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locale`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: '@wapps/gatsby-plugin-i18next',
+      options: {
+        availableLngs: ['en', 'ru', 'by'],
+        fallbackLng: 'en',
+        i18nextOptions: {
+          debug: false,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
