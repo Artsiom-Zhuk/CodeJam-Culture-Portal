9176b5344de3ae27b347eb5dcaf588d28a008c1b
Mitskevich Victor <mv2.mx@yandex.ru>
2019-07-30 15:18:12 +0300

    fix: quotes

--- src/data/producers.en.js
+++ src/data/producers.en.js
@@ -8,10 +8,10 @@ const producersEn = [
       ['1947—1949', `Was an actor of the auxilary staff of the Film Actors' Theatre.`],
       ['1949—1952', 'Studied at the Moscow Financial Institute. In 1958 he graduated from the directing department of VGIK.'],
       ['1962—1967', 'From 1982, he worked at the Belarusfilm studio, in 1968-1979, at the Gorky Film Studio.'],
-      ['1964', 'Author of the of several stories in the film magazine "wick".'],
-      ['1957', 'In his first feature film "Tambu-Lambu" acted as a director and writer.'],
-      ['1966', `became the winner of the VKF prize in the nomination "feature film for children and youth" for the film "City of Masters". The film was later included in the magazine “Art of Cinema” among the best children's films in a hundred years of cinema existence.`],
-      ['1968', `Bychkov’s film “The Life and Ascension of Yuras Bratchik”, filmed in 1968, was put on the shelf in the USSR until 1989, but according to the Kino-Theater website, a copy of the film was purchased by the Vatican.`],
+      ['1964', 'Author of the of several stories in the film magazine «wick».'],
+      ['1957', 'In his first feature film «Tambu-Lambu» acted as a director and writer.'],
+      ['1966', `became the winner of the VKF prize in the nomination «feature film for children and youth» for the film «City of Masters». The film was later included in the magazine «Art of Cinema» among the best children's films in a hundred years of cinema existence.`],
+      ['1968', `Bychkov’s film «The Life and Ascension of Yuras Bratchik», filmed in 1968, was put on the shelf in the USSR until 1989, but according to the Kino-Theater website, a copy of the film was purchased by the Vatican.`],
       ['2004', 'Buried in Moscow on the Vagankovo ​​cemetery.']
     ],
     workList: [
@@ -47,10 +47,10 @@ const producersEn = [
       ['1963 - 1968', `After school, he studied at the courses of amateur art leaders and at the ballet school. Army service held in Uruchye.`],
       ['1968', `Аfter demobilization, he worked at the Belarusfilm studio as an assistant to the artist, then as the head of the amateur film studio Zvezda of the Minsk ALC.`],
       ['1969', 'He entered VGIK on the department of directing feature films and television films.'],
-      ['1975', `Graduate diploma, thesis - the short feature film "Alekhine - a man" road "."`],
-      ['1976', `Worked at the studio "Letopis" film studio "Belarusfilm" and on television in the Belvideocentre.`],
-      ['In the middle of 80-th',  `Tried to start a career in fiction film. He put on a short feature film “Sergeant” about the war in Afghanistan, for which he received a prize at the Minsk festival of young filmmakers. The film was included in the movie almanac "The Bridge" and released on the All-Union screen.`],
-      ['From 2012 to 2016', `Wrote articles with memoirs for the newspaper "Sovetskaya Belorussia".`],
+      ['1975', `Graduate diploma, thesis - the short feature film «Alekhine - a man «road».»`],
+      ['1976', `Worked at the studio «Letopis» film studio «Belarusfilm» and on television in the Belvideocentre.`],
+      ['In the middle of 80-th',  `Tried to start a career in fiction film. He put on a short feature film «Sergeant» about the war in Afghanistan, for which he received a prize at the Minsk festival of young filmmakers. The film was included in the movie almanac «The Bridge» and released on the All-Union screen.`],
+      ['From 2012 to 2016', `Wrote articles with memoirs for the newspaper «Sovetskaya Belorussia».`],
       ['3 october 2017', 'Died He was buried next to his parents in the village Chernevka.']
     ],
     workList: [
@@ -84,7 +84,7 @@ const producersEn = [
       ['1950 - 1962', `He studied at school number 42 in Minsk. He graduated from the three courses of the Belarusian Theater and Art Institute in Minsk.`],
       ['1962', `He worked as a laborer at Belarusfilm, then as an assistant director, as an assistant to Vladimir Bychkov.`],
       ['1962', `After several attempts, he entered the directing department of VGIK (taking into account the previous education, he was accepted immediately to the second course).`],
-      ['From 1969', `director at the studio "Belarusfilm". After the first films made, the right to film The Last Summer of Childhood was almost accidental - after the untimely death on February 12, 1974, of director N. A. Kalinin. He was artistic director of "Belarusfilm".`],
+      ['From 1969', `director at the studio «Belarusfilm». After the first films made, the right to film The Last Summer of Childhood was almost accidental - after the untimely death on February 12, 1974, of director N. A. Kalinin. He was artistic director of «Belarusfilm».`],
       ['From 1990', 'director at Mosfilm.'],
       ['1990 - 1999', `taught at VGIK, the Institute of Contemporary Art in Moscow, at the Higher Directing Courses, at the All-Russian Institute of Retraining and Advanced Training of Cinematographic Workers.`],
       ['In 1999', 'headed the studio directing film and television at the Institute of Contemporary Art, since 2004 - Dean of the Faculty of Cinema and TV directing there.']
@@ -128,8 +128,8 @@ const producersEn = [
       ['In 1998', `debuted the full-length feature film Fall Up. I made fiction and documentary films in the creative association BelVideo Center.`],
       ['Since 1996', `Elena Trofimenko is a member of the Belarusian Union of Cinematographers.`],
       ['Since 1998', `was a member of the board of this organization.`],
-      ['', `" Elena Nikolaevna Trofimenko follows the path of cinema, called metaphorical, associative. "(A. Bobkova, film critic, article in the Belarusian cinema: young directors, biofilmography catalog Minsk, 1999)`]
-      ['In 2012', `published a collection of poems and reproductions -" Book "by Elena Trofimenko.`]
+      ['', `«Elena Nikolaevna Trofimenko follows the path of cinema, called metaphorical, associative.» (A. Bobkova, film critic, article in the Belarusian cinema: young directors, biofilmography catalog Minsk, 1999)`]
+      ['In 2012', `published a collection of poems and reproductions -«Book» by Elena Trofimenko.`]
     ],
     workList: [
       ['1991', 'And still the birds sing ...'],
@@ -161,15 +161,15 @@ const producersEn = [
       ['1992', 'In his youth, he was the champion of Minsk in wushu (the coach is the father). He graduated from the Lyceum of the Belarusian State University.'],
       ['2001', 'Graduated with honors (with a red diploma) from the BSU, specialization - administrative law, political science. Graduate School of Journalism, BSU.'],
       ['2002', `He went through a director's internship at the Moscow Art Theater. Chekhov, where his teacher was the People's Artist of the USSR Oleg Tabakov.`],
-      ['From 2001 to 2002', 'worked as a lawyer in the international law firm "Vlasov and Partners", as well as assistant chairman of the Belarusian Union of Theater Workers (served as press secretary and head of a number of art projects of the Union).'],
-      ['Since 2002', 'regular columnist of the reputable political and economic weekly journal “Belorusskaya Gazeta” (wrote more than 150 articles); and the Chief Editor of National Television (ONT), oversaw several media projects of the channel.'],
-      ['2001', `had already been staged at the Moscow Art Theater Chekhov, at the age of 22, staged the play “Lost Paradise” on the stage of the National Academic Theater named after Yanka Kupala (premiered on November 9, 2002).`],
-      ['June 21, 2003', 'on the stage of the Moscow Art Theater Chekhov held the premiere of the play “The Piedmont Beast” based on the play by Kureichik. '],
+      ['From 2001 to 2002', 'worked as a lawyer in the international law firm «Vlasov and Partners», as well as assistant chairman of the Belarusian Union of Theater Workers (served as press secretary and head of a number of art projects of the Union).'],
+      ['Since 2002', 'regular columnist of the reputable political and economic weekly journal «Belorusskaya Gazeta» (wrote more than 150 articles); and the Chief Editor of National Television (ONT), oversaw several media projects of the channel.'],
+      ['2001', `had already been staged at the Moscow Art Theater Chekhov, at the age of 22, staged the play «Lost Paradise» on the stage of the National Academic Theater named after Yanka Kupala (premiered on November 9, 2002).`],
+      ['June 21, 2003', 'on the stage of the Moscow Art Theater Chekhov held the premiere of the play «The Piedmont Beast» based on the play by Kureichik. '],
       ['Since 2007', 'works as a screenwriter in the movies.'],
       ['In 2003', `worked as editor of the first Belarusian television series' Hotel of wish fulfillment', CJSC Telefilm. On the basis of the Belarusian State Academy of Arts founded the Center for Contemporary Drama and Direction.`],
       ['In 2004', 'took part in an internship for managers in the field of management of non-profit organizations in the United States, conducted by the US Department of State.'],
       ['In 2006', 'was the editor-in-chief of the Vestnik Kultury newspaper.'],
-      ['In 2007', 'he moved from Minsk to the village near Smilovichi, from where his parents bought a house there. In this house the script for the film “Love-carrot-2” was written. '],  
+      ['In 2007', 'he moved from Minsk to the village near Smilovichi, from where his parents bought a house there. In this house the script for the film «Love-carrot-2» was written. '],  
       ['In 2010', 'I sold the house, and bought a spacious apartment in Minsk, returning back to my capital with my family.'],
       ['In 2012', 'together with Dmitry Friga, director Dmitry Marinin, cameramen Alexei Korneyev, Artyom Yakimov and Nikita Pinigin founded the creative association Bez Buslou Arts.'],
       ['', 'Gives master classes in different countries, taught scriptwriting at several universities in Russia, Belarus, and Lithuania.']
