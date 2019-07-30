19aa2b50973aaa35018a5b1e3e7df966ceb2328e
Volha Zakharava <zakharava.volha@gmail.com>
2019-07-30 23:28:13 +0300

    fix: change dev-team page according translation feature

--- src/pages/dev-team.js
+++ src/pages/dev-team.js
@@ -1,19 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout-example/layout";
+import { Translation } from 'react-i18next';
+import { withI18next } from '@wapps/gatsby-plugin-i18next';
+// import { TimeLineFull } from '../components/timeline/timeLine';
+import LandingPage from './landingPage/landingPage';
 import { Developers } from '../components/developers'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-    <Developers />
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  <Translation>
+    {t => (
+      <>     
+        <LandingPage/>
+        <Developers />
+        </>
+    )}
+  </Translation>
+);
 
-export default IndexPage
+export default withI18next()(IndexPage);
+
+export const query = graphql`
+  query($lng: String!) {
+    locales: allLocale(filter: { lng: { eq: $lng }, ns: { eq: "messages" } }) {
+      ...LocaleFragment
+    }
+  }
+`;
+
+        // {/* <h1>{t('Film directors of Belarus')}</h1>
+        // <p>{t('Welcome to your new Gatsby site.')}</p>
+        // <p>{t('Now go build something great.')}</p>
+        // <Link to="/page-2/">{t('Go to page 2')}</Link>
+        // <Link to="/timeline-example/">{t(' Timeline')}</Link>
+        // <iframe src='https://yandex.ru/map-widget/v1/?um=constructor%3Aca0787145253f3a3b70dcdfab8a861be037dc6d387c377b30882bd86d3d807b5&amp;source=constructor' width='100%' height='400' frameborder='0'></iframe>
+        // <a href="https://www.youtube.com/watch?v=9FZehV2qJTk" data-lity="data-lity">1</a> */}
