600a00795e49cd06e8a57d51e38f860ddadecccd
koverboy <koverboy@mail.ru>
2019-07-31 16:31:45 +0300

    feat:create component

---
+++ src/components/dayProducer/dayProducer.js
@@ -0,0 +1,24 @@
+import React from "react"
+import producers from "../../data/producers"
+import "./dayProducer.scss"
+
+function DayProducer() {
+  let now = new Date()
+  let ID
+  ID = now.getDay() % producers.length;
+
+  return (
+    <div class="dayProducer">
+      <a class="dayProducer_link" href='#'><img class="dayProducer_image" src={producers[ID].photoUrl}/></a>
+      <div class="dayProducer_bio">
+        <p> {producers[ID].name}</p>
+        <p>Date:{producers[ID].dates}</p>
+        <p>   {producers[ID].workList}</p>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default DayProducer
