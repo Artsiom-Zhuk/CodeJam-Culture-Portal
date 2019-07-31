e791176e1a3440fecd4bd25923814260d2001662
lexBogos <lex.bogos@gmail.com>
2019-07-31 12:40:35 +0300

    feat: add geoWidget

--- src/pages/directorPage/directorPage.js
+++ src/pages/directorPage/directorPage.js
@@ -4,18 +4,19 @@ import { graphql } from 'gatsby';
 import { Translation } from 'react-i18next';
 import { withI18next } from '@wapps/gatsby-plugin-i18next';
 import producers from '../../data/producers';
+import GeoWidget from '../../components/geoWidget/geoWidget';
 
 class DirectorPage extends React.Component{
     directorDescription = () => {
       const id = this.props.location.state.id;
       const info = <>
                     <h2>{producers[id].name}</h2>
+                    <GeoWidget mapCoord = {producers[id].mapPoints}/>
                   </>
       return info            
     }
 
     render(){ 
-      console.log(this.props.location.state.id)
         return (
             <Translation>
             {t => (
