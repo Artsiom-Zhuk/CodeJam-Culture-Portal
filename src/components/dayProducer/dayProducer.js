b0cffa2cfc0ecb92c3037c02157146b563757aac
koverboy <koverboy@mail.ru>
2019-07-31 16:35:57 +0300

    feat:restructure styles

--- src/components/dayProducer/dayProducer.js
+++ src/components/dayProducer/dayProducer.js
@@ -5,18 +5,16 @@ import "./dayProducer.scss"
 function DayProducer() {
   let now = new Date()
   let ID
-  ID = now.getDay() % producers.length;
+  ID = now.getDay() % producers.length
 
   return (
     <div class="dayProducer">
-      <a class="dayProducer_link" href='#'><img class="dayProducer_image" src={producers[ID].photoUrl}/></a>
+      <a class="dayProducer_link" href='#'><img class="dayProducer_link__image" src={producers[ID].photoUrl}/></a>
       <div class="dayProducer_bio">
         <p> {producers[ID].name}</p>
         <p>Date:{producers[ID].dates}</p>
         <p>   {producers[ID].workList}</p>
       </div>
-
-
     </div>
   )
 }
