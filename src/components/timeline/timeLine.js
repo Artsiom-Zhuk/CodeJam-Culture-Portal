91a2b1b89efa099f13c1875d55eba6c7353176da
Siarhey Kiniou <netralitet@gmail.com>
2019-07-31 10:41:19 +0300

    feat: applay translate for timeline

--- src/components/timeline/timeLine.js
+++ src/components/timeline/timeLine.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Timeline }  from 'vertical-timeline-component-for-react';
-import { TimeItem } from './timeline-item/TimeLineItem';
+import TimeItem from './timeline-item/TimeLineItem';
 
 export const TimeLineFull = ( {producer} ) => {
   const biographyData = producer.biography;
--- src/components/timeline/timeline-item/TimeLineItem.js
+++ src/components/timeline/timeline-item/TimeLineItem.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { TimelineItem }  from 'vertical-timeline-component-for-react';
+import { Translation } from 'react-i18next';
+import { withI18next } from '@wapps/gatsby-plugin-i18next';
 import './TimeLineItem.scss';
 
-export const TimeItem = ({ biography }) => {
+const TimeItem = ({ biography }) => {
   
   const elements = biography.map((item, index) => {
     return (
+      <Translation>
+      {t => (
       <TimelineItem
       key={ index }
       dateText={ item[0] }
     >
-      <h3>{ item[0] }</h3>
-      <p>{ item[1] }</p>
+      <h3>{item[0] }</h3>
+      <p>{ t(item[1]) }</p>
     </TimelineItem>
-    );
+     )}
+     </Translation>
+    )
+    
   });
 
   return (
@@ -22,3 +29,13 @@ export const TimeItem = ({ biography }) => {
     </div>
   );
 };
+
+export default TimeItem
+
+export const query = graphql`
+  query($lng: String!) {
+    locales: allLocale(filter: { lng: { eq: $lng }, ns: { eq: "messages" } }) {
+      ...LocaleFragment
+    }
+  }
+`;
