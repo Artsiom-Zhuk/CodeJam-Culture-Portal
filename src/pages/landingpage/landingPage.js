5cd74cca437a233a64d39d54f48353db507ef340
koverboy <koverboy@mail.ru>
2019-07-31 16:32:58 +0300

    feat:add component to the landingPage

--- src/pages/landingpage/landingPage.js
+++ src/pages/landingpage/landingPage.js
@@ -1,14 +1,19 @@
 import React from "react"
 // import { Translation } from 'react-i18next';
 // import { Link, withI18next } from '@wapps/gatsby-plugin-i18next';
-import Header from '../../components/header-example/header';
+import Header from "../../components/header-example/header"
+import DayProducer from "../../components/dayProducer/dayProducer"
 
-class LandingPage extends React.Component{
-    render(){
-       return   <Header/>
-                
-    }
+
+class LandingPage extends React.Component {
+  render() {
+    return (<>
+        <Header/>
+        <DayProducer/>
+      </>
+    )
+  }
 }
 
 
-export default LandingPage;
+export default LandingPage
