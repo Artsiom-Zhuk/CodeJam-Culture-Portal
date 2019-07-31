faf750c75cd407378ead203f295f0202a6af2d7a
lexbogos <you@example.com>
2019-07-31 18:24:40 +0300

    fix delete unused comp

--- src/pages/producer.js
+++ src/pages/producer.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Header from '../components/header-example/header';
 import { graphql } from 'gatsby';
 import { Translation } from 'react-i18next';
 import { withI18next } from '@wapps/gatsby-plugin-i18next';
@@ -8,15 +7,20 @@ import Layout from "../components/layout/layout"
 import { TimeLineFull } from '../components/timeline/timeLine';
 import PhotosGallery from '../components/photo-gallery';
 import VideoWidget from '../components/videoWidget';
+import GeoWidget from '../components/geoWidget/geoWidget';
 
 const ProducerPage = (props) => {
   const id= (props.location.state.id ? props.location.state.id: 0);
   const producer = producers[id];
+  const mapPoints = producers[id].mapPoints||[[55.75, 37.57]];
+  const videoUrl = producers[id].videoUrl.slice(32,)
   return (
     <Translation>
       {t => (
       <Layout>
         <h2>{t(producer.name)}</h2>
+        <GeoWidget mapPoints = {mapPoints}/>
+        <VideoWidget videoId = {videoUrl}/>
         <TimeLineFull producer = { producer }/>
         <PhotosGallery producer = { producer }/>
         <VideoWidget producer = { producer }/>
