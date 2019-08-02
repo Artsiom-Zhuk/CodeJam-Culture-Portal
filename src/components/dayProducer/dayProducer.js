60e7d5c62e4218c9d5d00963e3bd9f20d80ed95a
Mitskevich Victor <mv2.mx@yandex.ru>
2019-08-02 09:47:17 +0300

    fix: warnings

--- src/components/dayProducer/dayProducer.js
+++ src/components/dayProducer/dayProducer.js
@@ -1,21 +1,21 @@
-import React from "react"
-import producersEn from "../../data/producers"
-import "./dayProducer.scss"
+import React from "react";
+import producers from "../../data/producers";
+import "./dayProducer.scss";
 
 function DayProducer() {
-  let now = new Date()
-  let ID
-  ID = now.getDay() % producersEn.length
+  let now = new Date();
+  let ID;
+  ID = now.getDay() % producers.length;
 
   return (
-    <div class="dayProducer">
-      <a class="dayProducer_link" href='#'><img class="dayProducer_link__image" src={producersEn[ID].photoUrl}/></a>
-      <div class="dayProducer_bio">
-        <p> {producersEn[ID].name}</p>
-        <p>{producersEn[ID].dates}</p>
+    <div className="dayProducer">
+      <a className="dayProducer_link" href='#'><img className="dayProducer_link__image" src={producers[ID].photoUrl} alt='ProducerOfTheDay'/></a>
+      <div className="dayProducer_bio">
+        <p> {producers[ID].name}</p>
+        <p>{producers[ID].dates}</p>
       </div>
     </div>
   )
 }
 
-export default DayProducer
+export default DayProducer;
